7b7409f6a397d0ed4089625f9b0a13fe2927e1e1
EnesK41 <fire.enes@hotmail.com>
2024-11-09 14:47:13 +0300

    Added email and phone fields to the registration form

--- dentist_website/users/forms.py
+++ dentist_website/users/forms.py
@@ -1,12 +1,25 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 from .models import Profile, CustomUser
 
 class UserRegisterForm(UserCreationForm):
     
-    #Can add email or phone fields
+    email = forms.EmailField(required=False)  # Optional field
+    phone = forms.CharField(max_length=15, required=False)
     
     class Meta:
         model = CustomUser
-        fields = ['username', 'password1', 'password2']
+        fields = ['username','email','phone', 'password1', 'password2']
+        
+    def clean(self):
+        cleaned_data = super().clean()
+        email = cleaned_data.get("email")
+        phone = cleaned_data.get("phone")
+
+        # Check if at least one of email, or phone is provided
+        if not email and not phone:
+            raise ValidationError("Please provide at least one of email, or phone.")
+        
+        return cleaned_data
