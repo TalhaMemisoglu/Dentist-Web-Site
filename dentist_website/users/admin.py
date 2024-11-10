2a74ea3a6458e51e46f85ad27bc3b46ead1a0d7d
TalhaMemisoglu <talhamemisoglu0@gmail.com>
2024-11-10 14:27:41 +0300

    fixed 2

--- dentist_website/users/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Profile
-
-
-admin.site.register(Profile)        #Registering the Profile model to the admin panel
-
-#Gotta change the admin name and password inside terminal!
-#Temporary superuser : Admin , admin@gmail.com #password: enes4141
