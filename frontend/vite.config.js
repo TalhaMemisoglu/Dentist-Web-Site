86eaac25c565542d6cc820ff1688ee1e56b72454
EnesK41 <fire.enes@hotmail.com>
2024-11-25 17:12:43 +0300

    added our backend url as proxy to smoothen the communication between backend and frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig({   //Will send the request to the backend server
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': { // This matches any request starting with `/api`
+        target: 'http://127.0.0.1:8000', // Your backend URL
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
