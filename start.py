ed06fbf37444c76c8766e81b9650d9c83cbd1886
EnesK41 <fire.enes@hotmail.com>
2024-11-25 17:42:56 +0300

    doesn't work for now so dont use it

--- start.py
+++ start.py
@@ -1,27 +1,19 @@
-import os
+''' #comment section
 import subprocess
+import os
+import sys
 
-# Get the absolute paths of the directories
-backend_dir = os.path.abspath("backend")
-frontend_dir = os.path.abspath("frontend")
+def start_backend():
+    backend_dir = os.path.join(os.getcwd(), 'backend')
+    print(f"Starting backend in directory: {backend_dir}")
+    subprocess.Popen(['python', 'manage.py', 'runserver'], cwd=backend_dir)
 
-print(f"Backend directory: {backend_dir}")
-print(f"Frontend directory: {frontend_dir}")
+def start_frontend():
+    frontend_dir = os.path.join(os.getcwd(), 'frontend')
+    print(f"Starting frontend in directory: {frontend_dir}")
+    subprocess.Popen(['npm', 'run', 'dev'], cwd=frontend_dir, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-
-try:
-    # Start the backend
-    backend = subprocess.Popen(["python", "manage.py", "runserver"], cwd=backend_dir)
-
-    # Start the frontend
-    frontend = subprocess.Popen(["npm", "run", "dev"], cwd=frontend_dir)
-
-    # Wait for the processes to finish
-    backend.wait()
-    frontend.wait()
-except FileNotFoundError as e:
-    print(f"Error: {e}")
-except KeyboardInterrupt:
-    print("Shutting down...")
-    backend.terminate()
-    frontend.terminate()
+if __name__ == "__main__":
+    start_backend()
+    start_frontend() #|
+'''
